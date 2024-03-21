55b3127bb05275f872641089be5683e38ebb8ce1
“Justyn” <justyntkw@gmail.com>
2024-03-21 23:56:27 +0800

    change abit

--- Networking/Server/main.cpp
+++ Networking/Server/main.cpp
@@ -8,4 +8,7 @@ int main()
 // g++ Server/main.cpp Server/Server.cpp Server/SimpleServer.cpp Sockets/SimpleSocket.cpp Sockets/BindingSocket.cpp Sockets/ListeningSocket.cpp -o test
 // sudo ./test
 
-// Now needs to run sudo; prolly need to write a code that finds a non-priveleged free port (> 1024), rn port 0 means find a free port
+// sudo netstat -tulpn
+// checks for all processes connected to a port and their pid and local address
+
+// change port in Server.cpp to 80 or others if not working
