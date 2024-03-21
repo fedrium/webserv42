e67221b5423f6490d233cb2d24d0f68ae8aff8fb
“Justyn” <justyntkw@gmail.com>
2024-03-21 21:07:25 +0800

    add 1 line comment

--- Networking/Server/main.cpp
+++ Networking/Server/main.cpp
@@ -7,3 +7,5 @@ int main()
 
 // g++ Server/main.cpp Server/Server.cpp Server/SimpleServer.cpp Sockets/SimpleSocket.cpp Sockets/BindingSocket.cpp Sockets/ListeningSocket.cpp -o test
 // sudo ./test
+
+// Now needs to run sudo; prolly need to write a code that finds a non-priveleged free port (> 1024), rn port 0 means find a free port
