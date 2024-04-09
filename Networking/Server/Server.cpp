17f4362a0463d83ce610edf337b48bab5a273566
“Justyn” <justyntkw@gmail.com>
2024-04-10 02:48:47 +0800

    ok now we know how to send html files over to client browser

--- Networking/Server/Server.cpp
+++ Networking/Server/Server.cpp
@@ -30,15 +30,34 @@ void HDE::Server::handler()
 void HDE::Server::responder()
 {
 	char *text;
-	if (header[1] == "/cum")
-		text = (char *)"I will fking piss white cement it will gush out of my pipe";
-	else if (header[1] == "/wtf")
-		text = (char *)"Nah bruh tf is this holy bs wallahi im finished";
-	else if (header[1] == "/ded")
-		text = (char *)"*dies*";
+	if (header[1] == "/cum" || header[1] == "/wtf" || header[1] == "/bet")
+	{
+		if (header[1] == "/cum")
+			text = (char *)"I will fking piss white cement it will gush out of my pipe";
+		else if (header[1] == "/wtf")
+			text = (char *)"Nah bruh tf is this holy bs wallahi im finished";
+		else if (header[1] == "/bet")
+			text = (char *)"Always bet on hakari";
+		write(new_socket, text, strlen(text));
+	}
 	else
-		text = (char *)"Always bet on hakari";
-	write(new_socket, text, strlen(text));
+	{
+		const char htmlarr[] = "HTTP/1.1 200 OK\r\n"
+			"Connection: close\r\n"
+			"Content-type: text/html\r\n"
+			"\r\n"
+			"<html>\r\n"
+			"<head>\r\n"
+			"<title>BALLFART!</title>\r\n"
+			"</head>\r\n"
+			"<body>\r\n"
+			"<h3>Maisu to meetu yu, ballfart sensei</h3>\r\n"
+			"<h2>Ah yes, my narduo technique, havent used it in 10 years or so</h2>\r\n"
+			"<h1>Mud style no jutsu: explosive diarrhea cluster bomb</h1>\r\n"
+			"</body>\r\n"
+			"</html>\r\n\r\n";
+		send(new_socket, htmlarr, sizeof(htmlarr), 0);
+	}
 	close(new_socket);
 }
 
