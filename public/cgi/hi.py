c6da9f9fe281efc48b8ebf36ace36880e6dba45f
cyu-xian <thg7091@gmail.com>
2024-05-05 16:16:15 +0800

    done post login cgi

--- public/cgi/hi.py
+++ public/cgi/hi.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 
+import os
+
+env = os.environ
+
+vars = {}
+
+if env:
+    for key, value in env.items():
+        # Split each key-value pair using "=" delimiter
+        name, value = key, value
+        vars[name] = value
+
+username = vars.get("username", "")
+password = vars.get("password", "")
+
 print("<html>")
 print("<head>")
-print("<title>CGI Script in Python</title>")
+print("<title>HALLO</title>")
 print("</head>")
 print("<body>")
-print("<h1>Hello!</h1>")
+print("<h1>Hello " + username + "!</h1>")
+print("</br>")
+print("<h1>Your password is:" + password + ".</h1>")
 print("</body>")
 print("</html>")
+print('<button onclick="redirectToAnotherPage()">To Upload</button>')
+print("<script>")
+print("        function redirectToAnotherPage() {")
+print("            // Redirect to another HTML file")
+print("            window.location.href = 'upload.html';")
+print("        }")
+print("    </script>")
