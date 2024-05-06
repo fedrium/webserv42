74fb2dd9b04b7214d0364edf02da7a7a8fb7f520
“Justyn” <justyntkw@gmail.com>
2024-05-06 08:56:28 +0800

    fixed remove

--- Networking/Server/Delete.cpp
+++ Networking/Server/Delete.cpp
@@ -2,10 +2,15 @@
 
 void HDE::Server::handleDelete(int socket)
 {
-	(void)socket;
-	string command, deletePath;
-	deletePath = "./database/files" + header[1];
-	command = "rm " + deletePath;
-	system(command.c_str());
+	string deletePath;
+	deletePath = this->file_path;
+	if (access(deletePath.c_str(), 0) != 0)
+	{
+		send_error_page(socket, "404");
+		return;
+	}
+	else
+		cout << "[INFO] Removing item located at " << deletePath << endl;
+	remove(deletePath.c_str());
 	this->status = DONE;
 }
