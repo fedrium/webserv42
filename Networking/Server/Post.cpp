73fbd9eba2a82a4cf74a6128823159fc9bf4e151
“Justyn” <justyntkw@gmail.com>
2024-05-06 21:23:38 +0800

    cleaned abit

--- Networking/Server/Post.cpp
+++ Networking/Server/Post.cpp
@@ -12,23 +12,4 @@ void HDE::Server::handlePost(int socket)
 	// parsebody();
 	cgi(socket);
 	this->status = DONE;
-	// error(socket, "404");
 }
-
-// replace this whole thing and upload.py
-// void HDE::Server::cgi(int socket)
-// {
-// 	vector<string> args = chopString(content, "\r\n");
-// 	vector<string> tmp;
-// 	string command, filename;
-// 	for (int i = 0; i < args.size(); i++)
-// 		cout << args[i] << endl;
-// 	tmp = chopString(args[1], ";");
-// 	filename = chopString(tmp[tmp.size() - 1], "=")[1];
-// 	command = "python3 public/cgi/upload.py " + filename;
-// 	for (int i = 3; i < args.size() - 1; i++)
-// 		command.append(" ").append("\"").append(args[i]).append("\"");
-// 	cout << command << endl;
-// 	system(command.c_str());
-// 	png(socket, "/i_forgor.png");
-// }
